4877da21cb6b4a6715e0a8c2e857dff2228b95f6
Ritesh Kadmawala <ritesh@vertexcover.io>
2019-01-27 21:10:40 +0530

    add dockerfile and fix dependencies

--- shopify-crawler/cli.py
+++ shopify-crawler/cli.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python
-
+import os
 import asyncio
-import fire
+
+import click
 
 from shopify_crawler.datastore import FireStore
 from shopify_crawler.crawler import ShopifyAppCrawler
 
+MAX_WORKERS = os.environ.get("MAX_WORKERS", 10)
 
-def crawl(max_workers=10):
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("-w", "--max-workers", type=click.INT, default=MAX_WORKERS)
+def crawl(max_workers=MAX_WORKERS):
+    """
+    Start the shopify apps crawler
+    """
+    print(f"Crawling shopify apps with {max_workers} workers")
     loop = asyncio.get_event_loop()
     db = FireStore()
     crawler = ShopifyAppCrawler(db=db, max_workers=max_workers)
     loop.run_until_complete(crawler.run())
 
 
+@cli.group()
+def docker():
+    pass
+
+
 if __name__ == "__main__":
-    fire.Fire()
+    cli()
